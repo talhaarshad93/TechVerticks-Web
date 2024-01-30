fc77bdbe08ac1f53c9a3076d4ca58494796a6497
MohsinAliRizvi1991 <s.mohsinali91@gmail.com>
2024-01-30 18:49:09 +0500

    moshin addded 02

---
+++ pages/mohsin.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const mohsin = () => {
+  return (
+    <View>
+      <Text>mohsin</Text>
+    </View>
+  )
+}
+
+export default mohsin
+
+const styles = StyleSheet.create({})

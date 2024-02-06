13969fbd61f985bbc5e335cc5350a945d3217466
MohsinAliRizvi1991 <s.mohsinali91@gmail.com>
2024-02-06 19:23:12 +0500

    Navigation work

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import img from "../../assets/images/Logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavLink } from "react-bootstrap";
 import style from './Header.module.css'
 import classNames from "classnames";
+import Link from "next/link";
+import {useRouter} from "next/router";
+
 
 const Header = () => {
+  const router = useRouter()
+  const [menuOpen, setmenuOpen]=useState(false)
+  useEffect(() => {
+    // Close the menu when the route changes
+    setmenuOpen(false);
+  }, [router.pathname]);
   return (
     <Navbar collapseOnSelect expand="sm" bg="none" variant="light">
     <div className="container-fluid px-5  flex justify-content-between bg-dager">
      <div className="mt-3 ml-5"> <Navbar.Brand href="#">
-        <Image src={img} width={200} height={30} />
       </Navbar.Brand></div>
-      <div className="mr-5 mt-3">
+      {/* <div className="mr-5 mt-3">
       <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
@@ -25,7 +33,46 @@ const Header = () => {
           <Nav.Link className={classNames(style.fontWeight)} href="/BlogPage" style={{ marginLeft: 'auto' }}>Blog</Nav.Link>
           <Nav.Link className={classNames(style.fontWeight)} href="/Blog" style={{ marginLeft: 'auto' }}>Contact Us</Nav.Link>
         </Nav>
-      </Navbar.Collapse></div>
+      </Navbar.Collapse></div> */}
+      <div className={style.logo}>
+      <Image src={img} width={200} height={30} />
+
+      </div>
+      <div className={style.navbar}>
+
+          <nav>
+            <div className={style.menu} onClick={()=>{
+              setmenuOpen(!menuOpen);
+            }}>
+          <span></span>
+          <span></span>
+          <span></span>
+            </div>
+        <ul className={menuOpen ? style.open :""}>
+          <Link href={"/"} ><li
+                  className={
+                    router.pathname === "/" ? style.active : ""}>Home</li></Link>
+          <Link href={"/About"}><li
+                  className={
+                    router.pathname === "/About" ? style.active : ""}>About Us</li></Link>
+          <Link href={"/ServicePage"}><li
+                  className={
+                    router.pathname === "/ServicePage" ? style.active : ""}>Services</li></Link>
+          <Link href={"/Solutions"}><li
+                  className={
+                    router.pathname === "/Solutions" ? style.active : ""}>Solutions</li></Link>
+          <Link href={"/Portfolio"}><li
+                  className={
+                    router.pathname === "/Portfolio" ? style.active : ""}>Portfolio</li></Link>
+          <Link href={"/BlogPage"}><li
+                  className={
+                    router.pathname === "/BlogPage" ? style.active : ""}>Blog</li></Link>
+          <Link href={"/ContactUs"}><li
+                  className={
+                    router.pathname === "/ContactUs" ? style.active : ""}>Contact Us</li></Link>
+        </ul>
+          </nav>
+      </div>
     </div>
   </Navbar>
   );

4d15dde31824d381be2c16c1e71942ef7755e0a9
MohsinAliRizvi1991 <s.mohsinali91@gmail.com>
2024-02-12 16:50:05 +0500

    recent kate image change

--- components/Recent Work Component/RecentWork.jsx
+++ components/Recent Work Component/RecentWork.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ImgandBtn from './ImgandBtn'
-import mobGif from '../../assets/images/mob gif1.gif'
+// import mobGif from '../../assets/images/mob gif1.gif'
+import mobGif from '../../assets/images/kate.png'
 import mobGif2 from '../../assets/images/mob gif2.gif'
 import ltopGif from '../../assets/images/web gif.gif'
 import styles from './../We Build/Count.module.css'
@@ -16,6 +17,7 @@ const RecentWork = () => {
     <div className={style.main}>
       <div className={style.left}> 
             <ImgandBtn imageUrl ={mobGif2}  imageHeight={100} imageWidth={100} btnTxt={'Botiga Mobile App'}/>
+
             <ImgandBtn imageUrl ={ltopGif}  imageHeight={100} imageWidth={100} btnTxt={'Woo Space App'}/>
             {/* <ImgandBtn imageUrl = "../../assets/images/l.t.png" imageHeight={20} imageWidth={30}/> */}
       </div>

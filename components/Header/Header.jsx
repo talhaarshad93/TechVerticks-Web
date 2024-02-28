75618b1f538efdf1894878cba7e7797035942320
MohsinAliRizvi1991 <s.mohsinali91@gmail.com>
2024-02-28 20:04:53 +0500

    menu icon change

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import img from "../../assets/images/Logo.svg";
+import Home from "../../assets/images/Home.svg";
+import AboutUs from "../../assets/images/AboutUs.svg";
+import Services from "../../assets/images/Services.svg";
+import Solution from "../../assets/images/Solution.svg";
+import Portfolio from "../../assets/images/Portfolio.svg";
+import Blogs from "../../assets/images/Blogs.svg";
+import ContactUS from "../../assets/images/ContactUS.svg";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import style from './Header.module.css'
 import classNames from "classnames";
@@ -35,25 +43,25 @@ const Header = () => {
         <ul className={menuOpen ? style.open :""}>
           <Link href={"/"} ><li
                   className={
-                    router.pathname === "/" ? style.active : ""}>Home</li></Link>
+                    router.pathname === "/" ? style.active : ""}><Image src={Home} className={style.menuIconImg}/>Home</li></Link>
           <Link href={"/About"}><li
                   className={
-                    router.pathname === "/About" ? style.active : ""}>About Us</li></Link>
+                    router.pathname === "/About" ? style.active : ""}><Image src={AboutUs} className={style.menuIconImg}/>About Us</li></Link>
           <Link href={"/ServicePage"}><li
                   className={
-                    router.pathname === "/ServicePage" ? style.active : ""}>Services</li></Link>
+                    router.pathname === "/ServicePage" ? style.active : ""}><Image src={Services} className={style.menuIconImg}/>Services</li></Link>
           <Link href={"/Solutions"}><li
                   className={
-                    router.pathname === "/Solutions" ? style.active : ""}>Solutions</li></Link>
+                    router.pathname === "/Solutions" ? style.active : ""}><Image src={Solution} className={style.menuIconImg}/>Solutions</li></Link>
           <Link href={"/Portfolio"}><li
                   className={
-                    router.pathname === "/Portfolio" ? style.active : ""}>Portfolio</li></Link>
+                    router.pathname === "/Portfolio" ? style.active : ""}><Image src={Portfolio} className={style.menuIconImg}/>Portfolio</li></Link>
           <Link href={"/BlogPage"}><li
                   className={
-                    router.pathname === "/BlogPage" ? style.active : ""}>Blog</li></Link>
+                    router.pathname === "/BlogPage" ? style.active : ""}><Image src={Blogs} className={style.menuIconImg}/>Blog</li></Link>
           <Link href={"/ContactUs"}><li
                   className={
-                    router.pathname === "/ContactUs" ? style.active : ""}>Contact Us</li></Link>
+                    router.pathname === "/ContactUs" ? style.active : ""}><Image src={ContactUS} className={style.menuIconImg}/>Contact Us</li></Link>
           <Link href={"/ServicePagesDetails"}><li
                   className={
                     router.pathname === "/ContactUs" ? style.active : ""}>App Deve</li></Link>
